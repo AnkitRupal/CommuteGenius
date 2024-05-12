49d1708d571fd3200275ecd7ff5b410763c15913
Ankit Rupal <75999622+AnkitRupal@users.noreply.github.com>
2024-05-12 20:12:47 +0530

    Upload WebApp
    The previous commit failed due to battery failure of laptop. Only a fraction of the commit was pushed. Now uploading manually using github upload

---
+++ webapp/app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+import streamlit as st
+from PIL import Image
+
+
+st.set_page_config(layout="wide", page_title="Traffic Sign Detection")
+
+
+@st.cache(allow_output_mutation=True, show_spinner=False, suppress_st_warning=True)
+def load_model():
+    modelPath = './webapp/model.h5'
+    model=tf.keras.models.load_model(modelPath)
+    classes = { 
+                0:'Speed limit (20km/h)',
+                1:'Speed limit (30km/h)', 
+                2:'Speed limit (50km/h)', 
+                3:'Speed limit (60km/h)', 
+                4:'Speed limit (70km/h)', 
+                5:'Speed limit (80km/h)', 
+                6:'End of speed limit (80km/h)', 
+                7:'Speed limit (100km/h)', 
+                8:'Speed limit (120km/h)', 
+                9:'No passing', 
+                10:'No passing veh over 3.5 tons', 
+                11:'Right-of-way at intersection', 
+                12:'Priority road', 
+                13:'Yield', 
+                14:'Stop', 
+                15:'No vehicles', 
+                16:'Veh > 3.5 tons prohibited', 
+                17:'No entry', 
+                18:'General caution', 
+                19:'Dangerous curve left', 
+                20:'Dangerous curve right', 
+                21:'Double curve', 
+                22:'Bumpy road', 
+                23:'Slippery road', 
+                24:'Road narrows on the right', 
+                25:'Road work', 
+                26:'Traffic signals', 
+                27:'Pedestrians', 
+                28:'Children crossing', 
+                29:'Bicycles crossing', 
+                30:'Beware of ice/snow',
+                31:'Wild animals crossing', 
+                32:'End speed + passing limits', 
+                33:'Turn right ahead', 
+                34:'Turn left ahead', 
+                35:'Ahead only', 
+                36:'Go straight or right', 
+                37:'Go straight or left', 
+                38:'Keep right', 
+                39:'Keep left', 
+                40:'Roundabout mandatory', 
+                41:'End of no passing', 
+                42:'End no passing veh > 3.5 tons',
+            }
+    return (model, classes)
+
+
+def import_and_predict(image):
+    model, classes = load_model()
+    resize_image = image.resize((32, 32))
+    X_test = np.array([np.array(resize_image)])
+    X_test = X_test/255
+    try:
+        predictions = model.predict(X_test)
+        predicted_classes = np.argmax(predictions, axis=1)
+    except Exception as Error:
+        print(Error)
+        return "This image prediction is not supported"
+    return classes[predicted_classes[0]]
+
+
+st.write("""
+         # Traffic Sign Detection
+         """
+         )
+file = st.file_uploader("Please upload a Traffic Sign file", type=["png"])
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, caption = import_and_predict(image))
